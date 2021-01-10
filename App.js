5ade2ef48166cb88949a78036b4540755a264d55
slavkan777 <info.msdn.com@gmail.com>
2021-01-10 21:58:08 +0200

    Change title

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!111</Text>
+      <Text>Красава!</Text>
       <StatusBar style="auto" />
     </View>
   );
